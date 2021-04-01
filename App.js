828e8eba80b97a29578f4619ec7ebf7586af34d0
Mauricio Builes <rmbuilesm@unal.edu.co>
2021-04-01 01:11:49 -0500

    aplicacion finalizada

--- App.js
+++ App.js
@@ -1,21 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from "react-native";
+import axios from "axios";
+import * as React from "react";
+import { useEffect, useState } from "react";
+import { Appbar, Card, Title, Paragraph } from "react-native-paper";
 
-export default function App() {
+const App = () => {
+  const [usuario, setUsuario] = useState({});
+
+  async function obtenerUsuario() {
+    try {
+      const res = await axios.get(
+        "https://0q27loouph.execute-api.us-east-1.amazonaws.com/"
+      );
+      console.log(res.data);
+      setUsuario(res.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    obtenerUsuario();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Appbar.Header>
+        <Appbar.Content title="Mauricio Builes" />
+      </Appbar.Header>
+      <Card style={styles.centrar}>
+        <Card.Title title="Rest Nutrialus" />
+
+        <Card.Cover source={usuario.image} style={styles.imagen} />
+        <Card.Content>
+          <Title>
+            Name: <Paragraph>{usuario.name}</Paragraph>
+          </Title>
+          <Title>
+            Email: <Paragraph>{usuario.email}</Paragraph>
+          </Title>
+          <Title>
+            Phone: <Paragraph>{usuario.phone}</Paragraph>
+          </Title>
+          <Title>
+            Nutritionist: <Paragraph>{usuario.nutritionist}</Paragraph>
+          </Title>
+        </Card.Content>
+        <Card.Actions>
+          <Button title="Obtener  nuevo Paciente" onPress={obtenerUsuario}>
+            Ok
+          </Button>
+        </Card.Actions>
+      </Card>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  centrar: {
+    textAlign: "center",
+  },
+  imagen: {
+    height: "300px",
   },
 });
+
+export default App;
